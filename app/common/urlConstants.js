12c280bb395a7cb557a9d0067b47f53e5afc26bb
aboo <aboo@redmart.com>
2015-11-09 14:31:57 +0530

    new alpha index is added

--- app/common/urlConstants.js
+++ app/common/urlConstants.js
@@ -31,7 +31,7 @@ mainApp.constant('urlConstants', {
      "MAPPING_SIMPLE_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/page/_search?explain/eng_chn_keyword/_mapping",
      "MAPPING_ADV_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/page/_search?explain/hksearch/_mapping",
     //advsearchCtrl.js
-    "ADV_MAIN_URL" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/page/_search?explain/hksearch/page/_search?size=30",
+    "ADV_SEARCH_URL" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/page/_search?size=30",
     "ADV_SEARCH_ANALYSER_CJK" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/_analyze?analyzer=cjk",
     "ADV_SEARCH_ANALYSER_HAN" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/_analyze?analyzer=han_bigrams",
     "ADV_MAIN_URL" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/page/_search?size=30",
