87f8a0ad0b324643a55bd66c7db88612bc93199d
aboo <aboo@redmart.com>
2015-11-09 15:31:19 +0530

    new index changes

--- app/common/urlConstants.js
+++ app/common/urlConstants.js
@@ -28,8 +28,8 @@ mainApp.constant('urlConstants', {
     //homeCtrl.js
      "MAIN_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/_cat/indices/*?pretty",
      "HEALTH_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/page/_search?explain/_cat/health?v",
-     "MAPPING_SIMPLE_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/page/_search?explain/eng_chn_keyword/_mapping",
-     "MAPPING_ADV_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/page/_search?explain/hksearch/_mapping",
+     "MAPPING_SIMPLE_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/eng_chn_keyword/_mapping",
+     "MAPPING_ADV_URL" : "http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/_mapping",
     //advsearchCtrl.js
     "ADV_SEARCH_URL" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/page/_search?size=30",
     "ADV_SEARCH_ANALYSER_CJK" :"http://indexer.ecommerce-api.alpha.redmart.com/v1.0.0/hksearch/_analyze?analyzer=cjk",
